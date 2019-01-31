c20efb577405338c2802ddd7ac0958374dbf1fb1
Tomasz Załupski <tzalupski@gmail.com>
2019-01-31 19:21:00 +0100

    Change Devise's views to HAML. Allow user to login with username and email. Add more user model validations and tests.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,6 +10,13 @@ RSpec.describe User, type: :model do
     it { should validate_uniqueness_of(:username).case_insensitive }
     it { should validate_uniqueness_of(:email).case_insensitive }
   end
-
-  it { should validate_length_of(:password).is_at_least(6) }
+  
+  context "Username format" do
+    it { should allow_values('Abc_22', 'aaa', '21_37', '____', '000' , 'JP2', '_Plato_').for(:username) }
+    it { should_not allow_values('example1@gmail.com', '¯\_(ツ)_/¯', "( ͡° ͜ʖ ͡°)", '$eba').for(:username) }
+  end
+  context "Validate length" do
+    it { should validate_length_of(:username).is_at_least(3) }
+    it { should validate_length_of(:password).is_at_least(6) }
+  end
 end
