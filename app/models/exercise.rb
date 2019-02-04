0eeda1a09dc782c76e7fb236eb6eb899ce44c8d1
Tomasz Załupski <tzalupski@gmail.com>
2019-02-04 17:13:32 +0100

    Refactor model valdations.

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,7 +1,7 @@
 class Exercise < ApplicationRecord
   belongs_to :training
-  validates :name, presence: true
-  validates :reps, presence:true
+  validates_presence_of :name
+  validates_presence_of :reps
   validates_numericality_of :reps, greater_than_or_equal_to: 0  
   validates_numericality_of :weight, greater_than_or_equal_to: 0 
 end
--- app/models/training.rb
+++ app/models/training.rb
@@ -1,6 +1,6 @@
 class Training < ApplicationRecord
   belongs_to :user 
   has_many :exercises, dependent: :destroy
-  validates :date, presence: true
+  validates_presence_of :date
   validates_date :date, :between => [Date.today.prev_year(10), Date.today.next_year(10)]
 end
